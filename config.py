cf268df8cd1f40ab30fa8863d06e4debab30bd26
tilakpatidar <tilakpatidar@gmail.com>
2016-02-07 21:18:11 +0530

    Finished config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
-import json
+import json,log,sys
+c=None
 try:
 	c=json.loads(open("config.json","r").read())
+	log.put("Read config","SUCCESS")
 except Exception as e:
-	
+	log.put("Read config","FAIL")
+	sys.exit(0)
 def getConfig(key):
+	try:
+		return c[key]
+	except KeyError as e:
+		log.put(key+" not present in config","WARNING")
+		return None
